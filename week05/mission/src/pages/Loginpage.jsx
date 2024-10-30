61dcb94487b4e04e81c1f58ef030e78902c5647a
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2024-10-31 04:19:14 +0900

    1031

--- week05/mission/src/pages/Loginpage.jsx
+++ week05/mission/src/pages/Loginpage.jsx
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
-import EmailInput from '../components/Input/EmailInput';
-import PasswordInput from '../components/Input/PasswordInput';
+import InputField from '../components/Input/InputField';  // 새로 만든 InputField 컴포넌트
 import SubmitButton from '../components/Button/SubmitButton';
 
 const Background = styled.div`
@@ -22,30 +21,27 @@ const LoginContainer = styled.form`
   width: 50%;
   min-width: 600px;
   height: 400px;
-
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
 const LoginPage = () => {
-  //유효성 검사 스키마
+  // 유효성 검사 스키마
   const schema = yup.object().shape({
     email: yup.string().email("올바른 이메일 형식이 아닙니다! 다시 입력해주세요").required("이메일을 입력해주세요"),
     password: yup.string().min(8, "비밀번호는 8~16자 사이로 입력해주세요").max(16, "비밀번호는 8~16자 사이로 입력해주세요").required("비밀번호를 입력해주세요"),
   });
 
-
   const {
     register,
     handleSubmit,
-    formState: { errors,isValid },
+    formState: { errors, isValid },
     trigger,
   } = useForm({
     resolver: yupResolver(schema),
-    mode: "onBlur", // 제출 이전 유효성 검사
-    reValidateMode:"onChange" //제출 이후 유효성 검사 (onBlur 다음 이거로 변환 안됨)
-
+    mode: "onChange",
+    reValidateMode: "onChange",
   });
 
   const onSubmit = (data) => {
@@ -56,9 +52,24 @@ const LoginPage = () => {
     <Background>
       <LoginContainer onSubmit={handleSubmit(onSubmit)}>
         <h1>로그인</h1>
-        <EmailInput register={register} trigger={trigger} errors={errors} />
-        <PasswordInput register={register} trigger={trigger} errors={errors} />
-        <SubmitButton label="로그인" isvalidate={isValid}/>
+
+        <InputField
+          type="email"
+          name="email"
+          placeholder="이메일을 입력해주세요!"
+          register={register}
+          errors={errors}
+        />
+
+        <InputField
+          type="password"
+          name="password"
+          placeholder="비밀번호를 입력해주세요!"
+          register={register}
+          errors={errors}
+        />
+
+        <SubmitButton label="로그인" isvalidate={isValid} />
       </LoginContainer>
     </Background>
   );
--- week05/mission/src/pages/Signuppage.jsx
+++ week05/mission/src/pages/Signuppage.jsx
@@ -1,15 +1,97 @@
-import styled from 'styled-components'
-const Conatiner = styled.div`
-width:100vw;
-height:100vh;
-background-color:black;
-color:white;
-padding:20px;
-`
+import React from 'react';
+import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import InputField from '../components/Input/InputField';
+import SubmitButton from '../components/Button/SubmitButton';
 
-const SignupPage = () => {
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  color: white;
+  padding: 20px;
+  display: flex;
+  padding-top: 10%;
+  justify-content: center;
+`;
+
+const LoginContainer = styled.form`
+  width: 50%;
+  min-width: 600px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LoginPage = () => {
+  // 유효성 검사 스키마
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .email("올바른 이메일 형식이 아닙니다! 다시 입력해주세요")
+      .required("이메일을 반드시 입력해주세요."),
+    password: yup
+      .string()
+      .min(8, "비밀번호는 8자 이상이어야 합니다.")
+      .max(16, "비밀번호는 16자 이하여야 합니다.")
+      .required("비밀번호를 입력해주세요"),
+    passwordCheck: yup
+      .string()
+      .oneOf([yup.ref('password'), null], "비밀번호가 일치하지 않습니다.")
+      .required("비밀번호를 다시 입력해주세요"),
+  });
+
+  //useForm, 폼, 스키마 연결
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onBlur",
+    reValidateMode: "onChange",
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <Conatiner>회원가입 페이지에요</Conatiner>
-  )
-}
-export default SignupPage;
+    <Background>
+      <LoginContainer onSubmit={handleSubmit(onSubmit)}>
+        <h1>회원가입</h1>
+
+        <InputField
+          type="email"
+          name="email"
+          placeholder="이메일을 입력해주세요!"
+          register={register}
+          errors={errors}
+        />
+
+        <InputField
+          type="password"
+          name="password"
+          placeholder="비밀번호를 입력해주세요!"
+          register={register}
+          errors={errors}
+        />
+
+        <InputField
+          type="password"
+          name="passwordCheck"
+          placeholder="비밀번호를 다시 입력해주세요!"
+          register={register}
+          errors={errors}
+        />
+
+        <SubmitButton label="회원가입" isvalidate={isValid} />
+      </LoginContainer>
+    </Background>
+  );
+};
+
+export default LoginPage;
